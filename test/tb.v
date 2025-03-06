6d18a51d5631b3c1f394551dd21dd951cfd6a8aa
Devin <devinatkin@gmail.com>
2025-03-06 15:31:12 -0700

    Fix For Action

--- test/tb.v
+++ test/tb.v
@@ -27,7 +27,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_devinatkin_pwm user_project (
+  tt_um_devinatkin_vga user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
